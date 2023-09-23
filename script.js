64494c08ab6ebd75f1dd5e11d69e6098d5428292
osj2d <juniorsavi97@gmail.com>
2023-09-23 08:46:25 -0300

    ajustando eslint

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 function initHoraeData() {
   const horario = document.querySelector('[data-hora="hora"]');
   const data = document.querySelector('[data-data="data"]');
-
+  function verificaZero(n) {
+    return n < 10 ? `0${n}` : n;
+  }
+  function definirDia() {
+    let dia = new Date().getDate();
+    let mes = new Date().getMonth();
+    dia = verificaZero(dia);
+    mes = verificaZero(mes);
+    const ano = new Date().getFullYear();
+    data.innerHTML = `${dia}/${mes}/${ano}`;
+  }
+  function definirHorario() {
+    let hh = new Date().getHours();
+    let mm = new Date().getMinutes();
+    let ss = new Date().getSeconds();
+    hh = verificaZero(hh);
+    mm = verificaZero(mm);
+    ss = verificaZero(ss);
+    const horaFinal = `${hh}:${mm}<span class="segundos">${ss}<span>`;
+    horario.innerHTML = horaFinal;
+    definirDia();
+  }
   if (horario && data) {
-    function definirHorario() {
-      let hh = new Date().getHours();
-      let mm = new Date().getMinutes();
-      let ss = new Date().getSeconds();
-      hh = verificaZero(hh);
-      mm = verificaZero(mm);
-      ss = verificaZero(ss);
-      const horaFinal = `${hh}:${mm}<span class="segundos">${ss}<span>`;
-      horario.innerHTML = horaFinal;
-      definirDia();
-      var t = setTimeout(function () {
-        definirHorario();
-      }, 1000);
-    }
-
-    function definirDia() {
-      let dia = new Date().getDate();
-      let mes = new Date().getMonth();
-      dia = verificaZero(dia);
-      mes = verificaZero(mes);
-      const ano = new Date().getFullYear();
-      data.innerHTML = `${dia}/${mes}/${ano}`;
-    }
-    function verificaZero(n) {
-      return n < 10 ? "0" + n : n;
-    }
     definirHorario();
   }
+  setInterval(definirHorario, 1000);
 }
-
 initHoraeData();
